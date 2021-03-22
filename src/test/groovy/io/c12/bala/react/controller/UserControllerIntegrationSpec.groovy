368570169062eef2e3e1d691f45b2c480fa6e2a5
palanib2 <b.palaniappan@nationwide.com>
2021-03-22 00:27:33 -0500

    controller integration test added

--- src/test/groovy/io/c12/bala/react/controller/UserControllerIntegrationSpec.groovy
+++ src/test/groovy/io/c12/bala/react/controller/UserControllerIntegrationSpec.groovy
@@ -11,9 +11,12 @@ import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
+import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.web.reactive.function.BodyInserters
 import reactor.blockhound.BlockHound
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import spock.lang.Specification
 import spock.mock.DetachedMockFactory
 
@@ -40,6 +43,110 @@ class UserControllerIntegrationSpec extends Specification {
         BlockHound.install()
     }
 
+    def "add a new user returns Mono of userDto"() {
+        given: "create mock user to be saved"
+        UserDto userDto = UserDto.builder().firstName("John").lastName("Doe").userId("john.doe").emailId("john@cloud12.io").build()
+
+        when: "controller method is called to add a new user"
+        webTestClient
+                .post().uri("/api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(userDto))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody()
+                .jsonPath("\$.id").isEqualTo("NcyBK0F0Tbm38g8keKH30")
+
+        then: "verify if the repository and modelMapper are called"
+        1 * modelMapper.map(userDto, User.class) >> User.builder().firstName("John").lastName("Doe").userId("john.doe").emailId("john@cloud12.io").build()
+        1 * userRepository.save(_ as User) >> Mono.just(User.builder().id("NcyBK0F0Tbm38g8keKH30").firstName("John").lastName("Doe").userId("john.doe").emailId("john@cloud12.io").build())
+        1 * modelMapper.map(_ as User, UserDto.class) >> UserDto.builder().id("NcyBK0F0Tbm38g8keKH30").firstName("John").lastName("Doe").userId("john.doe").emailId("john@cloud12.io").build()
+    }
+
+    def "add a new user with missing required values"() {
+        given: "create mock user to be saved"
+        UserDto userDto = UserDto.builder().firstName("John").lastName("Doe").emailId("john@cloud12.io").build()
+
+        when: "controller method is called to add a new user"
+        webTestClient
+                .post().uri("/api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(userDto))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody()
+                .jsonPath("\$.error").isNotEmpty()
+                .jsonPath("\$.error").isEqualTo("Bad Request")
+                .jsonPath("\$.status").isEqualTo(400)
+                .jsonPath("\$.message").isEqualTo("")
+
+        then: "verify if the repository and modelMapper are NOT called"
+        0 * modelMapper.map(_, _)
+        0 * userRepository.save(_ as User)
+    }
+
+    def "update a user return a Mono of UserDto"() {
+        given: "create mock user to be updated"
+        UserDto userDto = UserDto.builder().firstName("John").lastName("Doe").userId("john.doe").emailId("john@cloud12.io").build()
+
+        when: "controller method is called to update a user"
+        webTestClient
+                .put().uri("/api/v1/users/{id}", "fzFIu8jUe09d2lXqJf9x5")
+                .body(BodyInserters.fromValue(userDto))
+                .exchange().expectStatus().isOk()
+                .expectBody()
+                .jsonPath("\$.id").isEqualTo("fzFIu8jUe09d2lXqJf9x5")
+
+        then: "verify if the repository and modelMapper are called"
+        1 * modelMapper.map(_ as UserDto, User.class) >> User.builder().id("fzFIu8jUe09d2lXqJf9x5").firstName("John").lastName("Doe").userId("john.doe").emailId("john@cloud12.io").build()
+        1 * userRepository.findById("fzFIu8jUe09d2lXqJf9x5") >> Mono.just(User.builder().id("fzFIu8jUe09d2lXqJf9x5").firstName("John").lastName("Doe").userId("john.doe").emailId("john@cloud12.io").build())
+        1 * userRepository.save(_ as User) >> Mono.just(User.builder().id("fzFIu8jUe09d2lXqJf9x5").firstName("John").lastName("Doe").userId("john.doe").emailId("john@cloud12.io").build())
+        1 * modelMapper.map(_ as User, UserDto.class) >> UserDto.builder().id("fzFIu8jUe09d2lXqJf9x5").firstName("John").lastName("Doe").userId("john.doe").emailId("john@cloud12.io").build()
+    }
+
+    def "update a user add interests return a Mono of UserDto"() {
+        given: "create mock user to be updated"
+        UserDto userDto = UserDto.builder().firstName("John").lastName("Doe").userId("john.doe").emailId("john@cloud12.io").interests(["iot", "home automation"]).build()
+
+        when: "controller method is called to update a user"
+        webTestClient
+                .put().uri("/api/v1/users/{id}", "fzFIu8jUe09d2lXqJf9x5")
+                .body(BodyInserters.fromValue(userDto))
+                .exchange().expectStatus().isOk()
+                .expectBody()
+                .jsonPath("\$.id").isEqualTo("fzFIu8jUe09d2lXqJf9x5")
+                .jsonPath("\$.interests").exists()
+                .jsonPath("\$.interests").isArray()
+
+        then: "verify if the repository and modelMapper are called"
+        1 * modelMapper.map(_ as UserDto, User.class) >> User.builder().id("fzFIu8jUe09d2lXqJf9x5").firstName("John").lastName("Doe").userId("john.doe").emailId("john@cloud12.io").interests(["iot", "home automation"]).build()
+        1 * userRepository.findById("fzFIu8jUe09d2lXqJf9x5") >> Mono.just(User.builder().id("fzFIu8jUe09d2lXqJf9x5").firstName("John").lastName("Doe").userId("john.doe").emailId("john@cloud12.io").build())
+        1 * userRepository.save(_ as User) >> Mono.just(User.builder().id("fzFIu8jUe09d2lXqJf9x5").firstName("John").lastName("Doe").userId("john.doe").emailId("john@cloud12.io").interests(["iot", "home automation"]).build())
+        1 * modelMapper.map(_ as User, UserDto.class) >> UserDto.builder().id("fzFIu8jUe09d2lXqJf9x5").firstName("John").lastName("Doe").userId("john.doe").emailId("john@cloud12.io").interests(["iot", "home automation"]).build()
+    }
+
+    def "update a user but user not found for the ID"() {
+        given: "create mock user to be updated"
+        UserDto userDto = UserDto.builder().firstName("John").lastName("Doe").userId("john.doe").emailId("john@cloud12.io").build()
+
+        when: "controller method is called to update a user"
+        webTestClient
+                .put().uri("/api/v1/users/{id}", "PsfcHujR0goR3KFHqIBIj")
+                .body(BodyInserters.fromValue(userDto))
+                .exchange().expectStatus().isNotFound()
+                .expectBody()
+                .jsonPath("\$.error").isNotEmpty()
+                .jsonPath("\$.error").isEqualTo("Not Found")
+                .jsonPath("\$.status").isEqualTo(404)
+                .jsonPath("\$.message").isEqualTo("")
+
+        then: "verify if the repository and modelMapper are called"
+        1 * modelMapper.map(_ as UserDto, User.class) >> User.builder().id("PsfcHujR0goR3KFHqIBIj").firstName("John").lastName("Doe").userId("john.doe").emailId("john@cloud12.io").build()
+        1 * userRepository.findById("PsfcHujR0goR3KFHqIBIj") >> Mono.empty()
+        0 * userRepository.save(_ as User)
+        0 * modelMapper.map(_ as User, UserDto.class)
+    }
+
     def "get all users from DB"() {
         when: "controller method is called to get all users"
         webTestClient
@@ -59,7 +166,7 @@ class UserControllerIntegrationSpec extends Specification {
     }
 
     def "get all users from DB returns two user"() {
-        when:
+        when: "controller method is called to get all users"
         webTestClient
                 .get().uri("/api/v1/users")
                 .exchange()
@@ -68,11 +175,67 @@ class UserControllerIntegrationSpec extends Specification {
                 .jsonPath("\$.[?(@.id=='6czbZTolSKSwf8P0jGXcb')]").exists()
                 .jsonPath("\$.[?(@.id=='9YX7n4AxOturpO3KNO3Pm')]").exists()
 
-        then:
+        then: "verify if the repository and modelMapper are called"
         1 * userRepository.findAll() >> Flux.just(User.builder().id("6czbZTolSKSwf8P0jGXcb").firstName("John").lastName("Doe").userId("john.doe").emailId("john.doe@c12.io").build(),
                 User.builder().id("9YX7n4AxOturpO3KNO3Pm").firstName("Jack").lastName("Reacher").userId("jack.reacher").emailId("jack.reacher@c12.io").build())
         2 * modelMapper.map(_ as User, UserDto.class) >> UserDto.builder().id("6czbZTolSKSwf8P0jGXcb").firstName("John").lastName("Doe").userId("john.doe").emailId("john.doe@c12.io").build() >>
                 UserDto.builder().id("9YX7n4AxOturpO3KNO3Pm").firstName("Jack").lastName("Reacher").userId("jack.reacher").emailId("jack.reacher@c12.io").build()
+    }
+
+    def "find user by id return Mono of UserDto"() {
+        when: "controller method is called to find a user by id"
+        webTestClient
+                .get().uri("/api/v1/users/{id}", "oApNCL9e3nfA7S6bQsz3m")
+                .exchange().expectStatus().isOk()
+                .expectBody()
+                .jsonPath("\$.id").isEqualTo("oApNCL9e3nfA7S6bQsz3m")
+
+        then: "verify if the repository and modelMapper are called"
+        1 * userRepository.findById("oApNCL9e3nfA7S6bQsz3m") >> Mono.just(User.builder().id("oApNCL9e3nfA7S6bQsz3m").firstName("John").lastName("Doe").userId("john.doe").emailId("john.doe@c12.io").build())
+        1 * modelMapper.map(_ as User, UserDto.class) >> UserDto.builder().id("oApNCL9e3nfA7S6bQsz3m").firstName("John").lastName("Doe").userId("john.doe").emailId("john.doe@c12.io").build()
+    }
+
+    def "find user by id when user by id not found"() {
+        when: "controller method is called to find a user by id"
+        webTestClient
+                .get().uri("/api/v1/users/{id}", "oApNCL9e3nfA7S6bQsz3m")
+                .exchange().expectStatus().isNotFound()
+                .expectBody()
+                .jsonPath("\$.error").isNotEmpty()
+                .jsonPath("\$.error").isEqualTo("Not Found")
+                .jsonPath("\$.status").isEqualTo(404)
+                .jsonPath("\$.message").isEqualTo("")
+
+        then: "verify if the repository and modelMapper are called"
+        1 * userRepository.findById("oApNCL9e3nfA7S6bQsz3m") >> Mono.empty()
+        0 * modelMapper.map(_ as User, UserDto.class)
+    }
+
+    def "delete user by id return No Content"() {
+        when: "controller method is called to delete a user by id"
+        webTestClient
+                .delete().uri("/api/v1/users/{id}", "2RtzDAA01725s_zGgcjlQ")
+                .exchange().expectStatus().isNoContent()
+
+        then: "verify if the repository and modelMapper are called"
+        1 * userRepository.findById("2RtzDAA01725s_zGgcjlQ") >> Mono.just(User.builder().id("2RtzDAA01725s_zGgcjlQ").firstName("John").lastName("Doe").userId("john.doe").emailId("john.doe@c12.io").build())
+        1 * userRepository.delete(_ as User) >> Mono.empty()
+    }
+
+    def "delete user by id when user by id not found"() {
+        when: "controller method is called to delete a user by id"
+        webTestClient
+                .delete().uri("/api/v1/users/{id}", "P3EvOAs9kM3BQ8H43kXkN")
+                .exchange().expectStatus().isNotFound()
+                .expectBody()
+                .jsonPath("\$.error").isNotEmpty()
+                .jsonPath("\$.error").isEqualTo("Not Found")
+                .jsonPath("\$.status").isEqualTo(404)
+                .jsonPath("\$.message").isEqualTo("")
+
+        then: "verify if the repository and modelMapper are called"
+        1 * userRepository.findById("P3EvOAs9kM3BQ8H43kXkN") >> Mono.empty()
+        0 * userRepository.delete(_ as User)
     }
 
     /**
