91f53e276dce324dc4ea1743e2cf32b7df503f3a
Balasubramanian Palaniappan <b.palaniappan@nationwide.com>
2021-11-28 16:09:47 -0600

    removed nanoid since secure random in linux is a blocking call

--- src/main/java/io/c12/bala/react/service/UserServiceImpl.java
+++ src/main/java/io/c12/bala/react/service/UserServiceImpl.java
@@ -3,7 +3,6 @@ package io.c12.bala.react.service;
 import io.c12.bala.react.dto.UserDto;
 import io.c12.bala.react.entity.User;
 import io.c12.bala.react.repository.UserRepository;
-import io.c12.bala.react.utils.NanoIdUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
@@ -12,6 +11,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -25,7 +26,7 @@ public class UserServiceImpl implements UserService {
     public Mono<UserDto> addUser(UserDto userDto) {
         log.info("Adding a user to db - {}", userDto);
         User user = modelMapper.map(userDto, User.class);       // convert DTO to entity before we store teh DB.
-        user.setId(NanoIdUtils.randomNanoId());
+        user.setId(UUID.randomUUID().toString());
         return userRepository.save(user).map(u -> modelMapper.map(u, UserDto.class));       // save and convert to UserDto class
     }
 
@@ -34,14 +35,14 @@ public class UserServiceImpl implements UserService {
         log.info("Update user. new user info {}", userDto);
         User user = modelMapper.map(userDto, User.class);       // convert DTO to entity before we update the DB.
         return findUserById(user.getId())
-                .flatMap(u -> userRepository.save(user))
-                .map(u -> modelMapper.map(u, UserDto.class));    // convert back to dto
+            .flatMap(u -> userRepository.save(user))
+            .map(u -> modelMapper.map(u, UserDto.class));    // convert back to dto
     }
 
     @Override
     public Mono<Void> deleteUser(String id) {
         return findUserById(id)
-                .flatMap(userRepository::delete);
+            .flatMap(userRepository::delete);
     }
 
     @Override
@@ -61,7 +62,7 @@ public class UserServiceImpl implements UserService {
 
     private Mono<User> findUserById(String id) {
         return userRepository.findById(id)
-                .switchIfEmpty(monoResponseStatusNotFoundException());      // Throws 404 exception if not found for id.
+            .switchIfEmpty(monoResponseStatusNotFoundException());      // Throws 404 exception if not found for id.
     }
 
 }
